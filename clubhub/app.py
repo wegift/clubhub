e3e5065afaba43023e5cc9fc80b286d30f740992
Will Pimblett <will@wegift.io>
2020-06-12 15:56:58 +0100

    Add sentry

--- clubhub/app.py
+++ clubhub/app.py
@@ -1,19 +1,31 @@
 import logging
+from os import environ
 
 import flask
+import sentry_sdk
 from flask import request
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from clubhub import gitlab, actions
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('message')
+logger.info('Booting clubhub')
+
+if environ.get('SENTRY_DSN'):
+    logger.info('Init Sentry')
+    sentry_sdk.init(
+        dsn=environ.get('SENTRY_DSN'),
+        integrations=[FlaskIntegration()]
+    )
 
 app = flask.Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "<h1>clubhub</h1><p>Hello World</p>"
+
 
 @app.route('/gitlab-webhook', methods=['POST'])
 def gitlab_webhook():
@@ -22,3 +34,8 @@ def gitlab_webhook():
     actions.on_gitlab_event(event)
     return {}
     # return jsonify(event)
+
+
+@app.route('/exc')
+def raise_exception():
+    raise Exception("Test exception")
